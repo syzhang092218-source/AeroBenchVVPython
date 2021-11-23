faffaae26322d2bedb790d36b0656195fc6e5c72
Songyuan Zhang <syzhang092218@gmail.com>
2021-11-23 13:41:52 -0500

    add setup.py

---
+++ code/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="aerobench",
+    version="0.0.0",
+    install_requires=[],
+    packages=find_packages(),
+)
